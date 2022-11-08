14a3cb706d33d3a73a7053d3d8974a2913ff1393
Andrey Vekselman <andrey.vekselman@gmail.com>
2022-11-08 23:05:52 +0200

    Add Rhombus
    * Add Rhombus Sequence

--- script.js
+++ script.js
@@ -160,3 +160,32 @@ for (let i = n; i >= 1; i--) {
 }
 Str += "<br><hr>";
 out.innerHTML = Str;
+////////////////////
+
+out = document.querySelector(".out9");
+Str = "";
+//////////// Rhombus
+Str = " Rhombus <br>";
+n = 5;
+for (let i = 1; i <= n; i++) {
+  for (let j = 1; j <= n - i; j++) {
+    Str += "&nbsp ";
+  }
+  for (let j = 1; j <= 2 * i - 1; j++) {
+    Str += "* ";
+  }
+
+  Str += "<br>";
+}
+for (let i = n - 1; i >= 1; i--) {
+  for (let j = 1; j <= n - i; j++) {
+    Str += "&nbsp ";
+  }
+  for (let j = 1; j <= 2 * i - 1; j++) {
+    Str += "* ";
+  }
+  Str += "<br>";
+}
+Str += "<br><hr>";
+out.innerHTML = Str;
+/////////////////////////
