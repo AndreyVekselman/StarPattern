e4e38575f6c47afcc12f0aae157ad032662e0696
Andrey Vekselman <andrey.vekselman@gmail.com>
2022-11-08 22:39:19 +0200

    Add Inverted Pyramid
    * Add inverted Pyramid sequence

--- script.js
+++ script.js
@@ -98,14 +98,33 @@ for (let i = 1; i <= n; i++) {
 }
 Str += "<br><hr>";
 out.innerHTML = Str;
+////////////
+
 out = document.querySelector(".out6");
 Str = "";
-
 //////////// Pyramid
 Str = " Pyramid <br>";
 n = 5;
-flag = n;
 for (let i = 1; i <= n; i++) {
+  for (let j = 1; j <= n - i; j++) {
+    Str += "&nbsp ";
+  }
+  for (let j = 1; j <= 2 * i - 1; j++) {
+    Str += "* ";
+  }
+
+  Str += "<br>";
+}
+Str += "<br><hr>";
+out.innerHTML = Str;
+/////
+
+out = document.querySelector(".out7");
+Str = "";
+//////////// Inverted Pyramid
+Str = " Inverted Pyramid <br>";
+n = 5;
+for (let i = n; i >= 1; i--) {
   for (let j = 1; j <= n - i; j++) {
     Str += "&nbsp ";
   }
