c7aa78c6552a37cb7043afc2cd246db765acadef
Andrey Vekselman <andrey.vekselman@gmail.com>
2022-11-08 22:54:06 +0200

    Add Hollow inverted Pyramid
    *Add Holllow inverted Pyramid sequence

--- script.js
+++ script.js
@@ -136,3 +136,27 @@ for (let i = n; i >= 1; i--) {
 }
 Str += "<br><hr>";
 out.innerHTML = Str;
+/////
+
+out = document.querySelector(".out8");
+Str = "";
+//////////// Hollow Inverted Pyramid
+Str = " Hollow Inverted Pyramid <br>";
+n = 5;
+for (let i = n; i >= 1; i--) {
+  for (let j = 1; j <= n - i; j++) {
+    Str += "&nbsp ";
+  }
+  for (let j = 1; j <= 2 * i - 1; j++) {
+    if (i == n) {
+      Str += "* ";
+    } else if (j == 1 || j == 2 * i - 1) {
+      Str += "* ";
+    } else {
+      Str += "&nbsp ";
+    }
+  }
+  Str += "<br>";
+}
+Str += "<br><hr>";
+out.innerHTML = Str;
