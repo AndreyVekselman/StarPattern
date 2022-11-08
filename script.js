643da36bb9d27c47cfb5a4cc3bbc1aa8b25a14b7
Andrey Vekselman <andrey.vekselman@gmail.com>
2022-11-08 21:24:43 +0200

    Add Pyramid
    *Add Pyramid sequence;

--- script.js
+++ script.js
@@ -98,4 +98,22 @@ for (let i = 1; i <= n; i++) {
 }
 Str += "<br><hr>";
 out.innerHTML = Str;
-///git training- push done with VSCode
+out = document.querySelector(".out6");
+Str = "";
+
+//////////// Pyramid
+Str = " Pyramid <br>";
+n = 5;
+flag = n;
+for (let i = 1; i <= n; i++) {
+  for (let j = 1; j <= n - i; j++) {
+    Str += "&nbsp ";
+  }
+  for (let j = 1; j <= 2 * i - 1; j++) {
+    Str += "* ";
+  }
+
+  Str += "<br>";
+}
+Str += "<br><hr>";
+out.innerHTML = Str;
